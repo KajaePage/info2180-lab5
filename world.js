1dacc48f11d7f8b3c18142360ed1b0ca3f26ac0c
KajaePage <91229936+KajaePage@users.noreply.github.com>
2021-11-08 17:25:37 -0500

    Added Exercise 5 Implementation

--- world.js
+++ world.js
@@ -9,7 +9,21 @@ function displaySearch(){
         let url = "http://localhost/info2180-lab5/world.php?country=";
         var input = document.getElementById("country").value;
         let query = Sanitizer(input);
-            let req = new Request(url+query);
+            let req = new Request(url+query+"&context=country");
+            
+            fetch(req)
+            .then(response => response.text())
+            .then(data => {
+                console.log(data)
+                document.getElementById("result").innerHTML = data;
+            });
+       
+    };
+    document.getElementById('lookupc').onclick = function() {
+        let url = "http://localhost/info2180-lab5/world.php?country=";
+        var input = document.getElementById("country").value;
+        let query = Sanitizer(input);
+            let req = new Request(url+query+"&context=cities");
             
             fetch(req)
             .then(response => response.text())
